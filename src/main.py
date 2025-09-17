263692da233117d3b02e9e2a2b9468cb5d8db721
Ryan Goldberg <ryan.s.goldberg@gmail.com>
2025-09-17 11:04:06 -0400

    Added a basic rosbag reader * Parses the metadata * Iterates over the messages and prints relevant fields / saves the images * Annotates images with basic info for readablility * Usage: From a ros2 supporting container (ex. osrf/ros:rolling-desktop) python3 src/main.py ingest --src tests/data/sensor_data_20250916_161600/ --dest tests/output
    Signed-off-by: Ryan Goldberg <ryan.s.goldberg@gmail.com>

--- src/main.py
+++ src/main.py
@@ -1,33 +1,42 @@
 from argparse import ArgumentParser
 
-SERVE='serve'
-PREPROCESS='preprocess'
+from ingest import ingest
+
+SERVE = "serve"
+INGEST = "ingest"
+
 
 def mosaic():
-    parser = ArgumentParser(description='Mosaic: A multi-tool for vehicular sensor data processing & visualization')
-    
-    # Common args
-    parser.add_argument('--dir', type=str, help='Path to a ROS bag containing directory')
-    
-    subparsers = parser.add_subparsers(required=True, dest='command')
+    parser = ArgumentParser(
+        description="Mosaic: A multi-tool for vehicular sensor data processing & visualization"
+    )
+
+    subparsers = parser.add_subparsers(required=True, dest="command")
 
     # Serve command args
-    serve_parser = subparsers.add_parser(SERVE, help='Serve an http xyz')
-    serve_parser.add_argument('--foo', type=str, help='')
+    serve_parser = subparsers.add_parser(SERVE, help="Serve an http xyz")
+    serve_parser.add_argument("--foo", type=str, help="")
 
-    # Preprocess command args
-    preprocess_parser = subparsers.add_parser(PREPROCESS, help='Preprocess xyz')
-    preprocess_parser.add_argument('--bar', type=str, help='')
-    
+    # Ingest command args
+    ingest_parser = subparsers.add_parser(INGEST, help="Ingest rosbags")
+    ingest_parser.add_argument(
+        "--src", type=str, help="Path to a ROS bag containing directory", required=True
+    )
+    ingest_parser.add_argument(
+        "--dest", type=str, help="Where to put the new rosbags", required=True
+    )
+
     args = parser.parse_args()
 
     if args.command == SERVE:
-        print(f'Serving')
-    elif args.command == PREPROCESS:
-        print(f'Preprocessing')
+        raise NotImplementedError
+    elif args.command == INGEST:
+        print(f"Ingesting")
+        ingest(bag_path=args.src, img_path=args.dest)
     else:
         parser.print_help()
         exit(1)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     mosaic()
